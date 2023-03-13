0f6612a4a17b56ec7c6930b1e36c38f261fbfa8a
wwendels <w.h.m.wendels@gmail.com>
2023-03-13 12:32:00 -0700

    update 20230313

--- ode2d_config.py
+++ ode2d_config.py
@@ -38,7 +38,7 @@ def getModel(case):
         xmesh = -100.0, 50.0, 30, 500, "$v$"
         ymesh = -50.0, 300.0, 30, 500, "$u$"
         tmesh = 500.,0.01 #200.,0.1
-        X0 = [np.array([-60,100])] #[np.array([-40,50])] #[np.array([-60,100])] # compared to (-60,0) in model A
+        X0 = [np.array([-60,100]), np.array([-70,100])] #[np.array([-40,50])] #[np.array([-60,100])] # compared to (-60,0) in model A
         eqtol = 1e-3
         model_lab = "IZH"
 
@@ -128,7 +128,7 @@ def getModel(case):
         xmesh = -0.5, 1.0, 30, 50, "$V$"
         ymesh = -0.05, 0.2, 30, 50, "$w$"
         tmesh = 500.,0.01
-        X0 = [np.array([0,0.15])]
+        X0 = [np.array([0,0.15]), np.array([0,0.2])]
         eqtol = 1e-3
         model_lab = "FHN"
 
--- ode545_main.py
+++ ode545_main.py
@@ -12,13 +12,15 @@ import ode2d_pplane as ode2d
 
 import ode545_functions as functions
 
+import pysindy as ps
+
 cwd = os.getcwd()
 
 # ===== INPUT ===== #
 
 # ----- model ----- #
-case = "IZH-B"                                                  # case label; will be searched for in the ode2d_config file
-savepath_main = cwd+"/IZH_model_B/"                             # main folder to save plots to; subfolders according to detailed settings will be made further below
+case = "FHN-A" #"IZH-B"                                                  # case label; will be searched for in the ode2d_config file
+savepath_main = cwd+"/FHN_model_A/"                             # main folder to save plots to; subfolders according to detailed settings will be made further below
 fmodel, xmesh, ymesh, tmesh, X0, eqtol = config.getModel(case)  # gets (detailed) input regarding model, mesh, initial value and tolerance for finding equilibria from ode2d_config
 tmesh = 1000.,0.01                                              # time mesh (different subfolders will be made for different time meshes)
 
@@ -26,11 +28,11 @@ tmesh = 1000.,0.01                                              # time mesh (dif
 # ymesh = -50.0, 300.0, 100, 500, "$u$"
 
 # ----- parameters input current function fI ----- #
-Iext = 100                                                      # input current
+Iext = 0 #100                                                      # input current
 tthres = 500                                                     # threshold current
 Pulses = [[180,200,Iext],[580,600,Iext]]                        # time intervals for current pulses 
 Iref = 0                                                        # reference current
-fI_lab = 2                                                      # choice for which current function (see output section for which label corresponds to which function)
+fI_lab = 1                                                      # choice for which current function (see output section for which label corresponds to which function)
 
 # ----- plotting ----- #
 opt_plot = 2                                                    # plotting option (1 = separate analysis plots, 2 = combined plots)
@@ -64,6 +66,7 @@ else:
 ### algorithm
 
 Xtraj, T = functions.trajectories(fmodel(fI), X0, tmesh, method="RK4")
+dt = T[1] - T[0]
 # Xtraj = Xtraj.T
 
 # initialization summary plot
@@ -76,6 +79,8 @@ for i in range(nt):
     Idata[0,i] = fI(T[i])
 print(Idata)
 
+models = []
+
 for i in range(len(X0)):
     height_ratios = [2]+[1 for i in range(n+1)]
     print(height_ratios)
@@ -85,22 +90,43 @@ for i in range(len(X0)):
     functions.plotTimeSeries(axs[1:n+1], Xtraj[i], T)
     functions.plotTimeSeries(axs[n+1], Idata, T, Xlims = [-0.1*Iext, 1.1*Iext])
 
-    fig.savefig(savepath+"/analysis_X0="+str(X0[i])+"-6", bbox_inches="tight")
+    fig.savefig(savepath+"/analysis_X0="+str(X0[i]).replace(".","")+"-FHN-A-1.png", bbox_inches="tight")
     plt.close()
 
 
+####################
+# PYSINDY RECOVERY #
+####################
+
+# train model for first X0
+model_ps = ps.SINDy()
+model_ps.fit(Xtraj[0].T, t=dt)
+model_ps.print()
+
+assert len(X0) > 1
+
+Xtraj_comp = model_ps.predict(Xtraj[1].T)
+# Xtraj_ref = model_ps.differentiate(Xtraj[1].T, t=dt)
+
+ndims = Xtraj_comp.shape[0]
+fig, axs = plt.subplots(ndims, 1, sharex=True, figsize=(7, 9))
+for i in range(ndims):
+    axs[i].plot(T, Xtraj_comp[:, i], 'k', label='numerical derivative')
+    # axs[i].plot(T, Xtraj_ref[:, i], 'r--', label='model prediction')
+    # axs[i].legend()
+    # axs[i].set(xlabel='t', ylabel='$\dot x_{}$'.format(i))
+fig.show()
 
 
-# print(X0)
-# # print(fmodel)
-# Xtraj, _ = ode2d.trajectories(fmodel(fI),X0,tmesh,method="RK4")
-# for i in range(len(X0)):
-#     savelabel_TSA_i = "TSA_X0="+str(X0[i])
-#     savelabel_PPA_i = "PPA_X0="+str(X0[i])
-#     print(Xtraj[i])
-#     functions.TSA(Xtraj[i], tmesh, savepath, savelabel_TSA_i)
-#     functions.PPA(fmodel(fI),xmesh,ymesh,Xtraj[i],savepath,savelabel_PPA_i)
-#     functions.plotI(fI,tmesh, savepath, savelabel_TSA_i)
 
-# plotI = [fI, "I", [0,Iext+10]] # information for plotting regarding input current function
-# analysis.phasePlaneAnalysis(fmodel(fI),xmesh,ymesh,tmesh,X0=X0,t=tmesh[0],plot_traj=2,eqtol=eqtol,opt_plot=opt_plot,legend=False,title=title,plotI=plotI,savepath=savepath,savelabel=savelabel)
+
+# # check
+# t_test_span = (t_test[0], t_test[-1])
+# x_test = solve_ivp(lorenz, t_test_span, x0_test, t_eval=t_test, **integrator_keywords).y.T  
+
+# # Compare SINDy-predicted derivatives with finite difference derivatives
+# print('Model score: %f' % model.score(x_test, t=dt))
+
+# print(models)
+
+
